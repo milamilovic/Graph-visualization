d1dd0eb99a51b0ae7cbe1c7566e5faf70b71c429
matejicdunja <dunja2002matejic@gmail.com>
2024-01-27 23:51:31 +0100

    deleted build folders

--- API/build/lib/services/data_source_api.py
+++
@@ -1,9 +0,0 @@
-from abc import abstractmethod
-
-from services.service_base_api import ServiceBase
-
-
-class GraphLoading(ServiceBase):
-    @abstractmethod
-    def load_graph(self, path: str) -> bool:
-        pass
--- API/build/lib/services/visualiser_api.py
+++
@@ -1,9 +0,0 @@
-from abc import abstractmethod
-
-from services.service_base_api import ServiceBase
-
-
-class GraphVisualisation(ServiceBase):
-    @abstractmethod
-    def visualize(self, graph, request):
-        pass
