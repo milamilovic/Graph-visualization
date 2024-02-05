02f0d8ea9002314daa2fb003f9b23c68aa9345d8
matejicdunja <dunja2002matejic@gmail.com>
2024-02-05 02:07:04 +0100

    showing edges without recursion

--- data_source_xml/load_xml/load_xml.py
+++ data_source_xml/load_xml/load_xml.py
@@ -9,7 +9,6 @@ from models.edge import Edge as e
 from models.graph import Graph as g
 
 
-
 class XmlLoader(GraphLoading):
     def __init__(self):
         self.node_id = 0
@@ -42,20 +41,19 @@ class XmlLoader(GraphLoading):
 
     def make_graph(self, data):
         self._graph = g()
-        nodes = self.create_airline_nodes(data)
+        nodes = self.create_nodes(data)
+        self.create_edges(nodes)
         return self._graph
 
-    def create_airline_nodes(self, data):
+    def create_nodes(self, data):
         nodes = []
         for item in data:
-            airline_node = n(None)
-            airline_node.id = self.get_next_node_id()
-
+            node = n(None)
+            node.id = self.get_next_node_id()
             for key, value in item.items():
-                airline_node.add_attribute(key, value)
-
-            nodes.append(airline_node)
-            self.add_node(airline_node)
+                node.add_attribute(key, value)
+            nodes.append(node)
+            self.add_node(node)
         return nodes
 
     def add_node(self, node):
@@ -67,3 +65,30 @@ class XmlLoader(GraphLoading):
         current_id = self.node_id
         self.node_id += 1
         return current_id
+
+    def get_next_edge_id(self):
+        current_id = self.edge_id
+        self.edge_id += 1
+        return current_id
+
+    def get_common_values(self, values_i, values_j):
+        if isinstance(values_i, (set, list)) and isinstance(values_j, (set, list)):
+            return set(values_i).intersection(set(values_j))
+        else:
+            if values_i == values_j:
+                return {values_i}
+        return set()
+
+    def create_edges(self, nodes):
+        for i, node_i in enumerate(nodes):
+            for j, node_j in enumerate(nodes[i + 1:], start=i + 1):
+                common_tags = set(node_i.tags()).intersection(node_j.tags())
+                for tag in common_tags:
+                    values_i = node_i.get_values_for_tag(tag)
+                    values_j = node_j.get_values_for_tag(tag)
+                    common_values = self.get_common_values(values_i, values_j)
+                    if common_values:
+                        if not self._graph.has_edge_between_nodes(node_i, node_j):
+                            edge = e(self.get_next_edge_id(), node_i, node_j, tag, False)
+                            node_i.add_edge(edge)
+                            self._graph.add_edge(edge)
